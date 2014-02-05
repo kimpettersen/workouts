1dc973072e091450cc2faf4ecf80deb03641382c
Kim Pettersen <kim.a.pettersen@gmail.com>
2014-02-05 08:38:26 +0000

    added username in header

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -2,6 +2,9 @@
 
 angular.module('myrunsApp')
   .controller('HeaderCtrl', function ($scope, $location, $http) {
+
+    $scope.user = localStorage.getItem('email');
+
     $scope.navigate = function(path) {
       if (path === '/logout') {
         $http.get(config.host + '/logout');
